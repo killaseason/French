175535bfe77520ff45321e8f995d2efccd7be732
killaseason <margrabe@gmail.com>
2017-07-02 14:48:51 -0400

    Nothing
    Nothing

---
+++ main.py
@@ -0,0 +1,7 @@
+#Loop over a range of dates
+
+#Retrieve a selection of documents
+
+##Loop over selection of documents, search for a term in each, and return links to files with hits and
+##the 10 words before and after the hits
+
